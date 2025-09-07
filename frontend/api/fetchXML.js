7efa025b92435ad344b7db9050d32c37b4fc111b
Андрей Валочкин <avalockin@gmail.com>
2025-09-07 23:45:37 +0300

    6

--- frontend/api/fetchXML.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import xml2js from "xml2js";
-
-const XML_URL = "https://i-maxi.com/ocext_yml_feed.xml";
-
-export async function fetchAndParseXML() {
-	const { data } = await axios.get(XML_URL);
-	return await xml2js.parseStringPromise(data);
-}
